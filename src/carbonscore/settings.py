a3069b1b4c1e7d8f620aa524ce188614d177d976
Raphael Wudy <raphael.wudy@th-rosenheim.de>
2024-04-06 15:59:48 +0200

    set up sonar, add gitignores and fix security issue with secret key

--- src/carbonscore/settings.py
+++ src/carbonscore/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n@m6(!1j=iupm7^)w(wnz^y3k_dktrn_r$##oa8wybrhu7%n2r'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
