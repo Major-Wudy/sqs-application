5228c5c0762564224d7ef895913325271d922e65
Raphael Wudy <raphael.wudy@th-rosenheim.de>
2024-04-11 21:39:27 +0200

    add fuel combustion service

--- src/application/models/fuel/fuel_combustion.py
+++ src/application/models/fuel/fuel_combustion.py
@@ -1,7 +1,7 @@
 from FuelSourceType import *
 
 class FuelCombustion:
-    def __init__(self, type, fuelSource_type, fuelSource_unit, consumptionValue):
+    def __init__(self, type, fuel_source_type, fuel_source_unit, consumption_value):
         self.type = type
         self.fuelSource_type = fuelSource_type
         self.fuelSource_unit = fuelSource_unit
---
+++ src/application/services/domain/fuel_combustion_service.py
@@ -0,0 +1,33 @@
+import sys
+import os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+application_dir = os.path.dirname(parent_dir)
+
+sys.path.append(parent_dir)
+sys.path.append(application_dir)
+
+from models.activity.activity_type import ActivityType
+from models.fuel.fuel_combustion import FuelCombustion
+from models.fuel.fuel_source_type import FuelSourceType
+from decimal import Decimal
+
+def create_fuel_combustion_entity(type: ActivityType, source_type_name: str, consumption_value: Decimal) -> FuelCombustion:
+    try:    
+        if not isinstance(type: ActivityType) or not isinstance(source_type_name: str) or not isinstance(consumption_value: Decimal):
+            raise TypeError()
+        
+        fuel = FuelSourceType()
+        fuel_unit = fuel.get_unit_by_name(source_type_name)
+        fuel_api_name = fuel.get_api_name_by_name(source_type_name)
+
+        if fuel_unit == ""
+            raise ValueError()
+        if fuel_api_name == ""
+            raise ValueError()
+
+        return FuelCombustion(ActivityType.FUEL_COMBUSTION, fuel_api_name, fuel_unit, consumption_value)
+    except TypeError:
+        print("Wrong parameters")
+    except ValueError:
+        print("Variables are empty")
