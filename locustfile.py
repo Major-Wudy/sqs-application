a71550109a9738f22b3aea6173490d893fcc9b4f
Raphael Wudy <raphael.wudy@th-rosenheim.de>
2024-05-30 21:16:31 +0200

    add host for load performance tests via docker containers

--- locustfile.py
+++ locustfile.py
@@ -1,14 +1,14 @@
 import time
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 from locust import HttpUser, task, between
 from json import JSONDecodeError
 
 class QuickstartUser(HttpUser):
     wait_time = between(1, 5)
-    token = os.environ.get('TOKEN_UNIT_TEST')
+    #token = os.environ.get('TOKEN_UNIT_TEST')
 
-    @task(1)
+    @task(2)
     def create_electricity(self):
         response = self.client.post("api/create/electricity/", json={"value":123.45, "country":"us","state":"fl","unit":"kwh"}, headers={'Authorization': 'Bearer ae7c53dcaafe8887d331003252fa90f6c5ff5059', "Content-Type": "application/json"})
         print("Response:", response.text)
@@ -20,14 +20,19 @@ class QuickstartUser(HttpUser):
         print("Response:", response.text)
         print("Response status code:", response.status_code)
     
-    @task(3)
+    @task(2)
     def create_shipping(self):
         response = self.client.post("api/create/shipping/", json={"weight_value":123.45,"weight_unit": "g","distance_value": 500.01,"distance_unit": "km","transport_method": "plane"}, headers={'Authorization': 'Bearer ae7c53dcaafe8887d331003252fa90f6c5ff5059', "Content-Type": "application/json"})
         print("Response:", response.text)
         print("Response status code:", response.status_code)
     
-    @task(4)
+    @task(2)
     def create_fuel(self):
         response = self.client.post("api/create/fuel/", json={"source":"Natural Gas","value":500}, headers={'Authorization': 'Bearer ae7c53dcaafe8887d331003252fa90f6c5ff5059', "Content-Type": "application/json"})
         print("Response:", response.text)
         print("Response status code:", response.status_code)
+
+    @task(1)
+    def create_estimate_electricity(self):
+        #elec_json = self.client.post("api/create/electricity/", json={"value":123.45, "country":"us","state":"fl","unit":"kwh"}, headers={'Authorization': 'Bearer ae7c53dcaafe8887d331003252fa90f6c5ff5059', "Content-Type": "application/json"})
+        #response = self.client.post("api/get/estimate/electricity/", json=elec_json, headers={'Authorization': 'Bearer ae7c53dcaafe8887d331003252fa90f6c5ff5059', "Content-Type": "application/json"})
