ffa6aa4d4af9bacba7af30df722cf5fe2c18b7f3
Raphael Wudy <raphael.wudy@th-rosenheim.de>
2024-06-04 09:43:09 +0200

    add database via docker

--- src/carbonscore/settings.py
+++ src/carbonscore/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'carbonscore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': '3306',
     }
 }
 
