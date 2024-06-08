70eba43d3f10104a54038e78542b261b4fd5323c
Raphael Wudy <raphael.wudy@th-rosenheim.de>
2024-06-08 14:43:52 +0200

    push to main

--- src/application/migrations/initdb.sql
+++
@@ -1,5 +0,0 @@
-USE sqs;
-
-CREATE USER 'docker'@'%' IDENTIFIED BY 'docker';
-GRANT ALL PRIVILEGES ON *.* TO 'docker'@'%';
-INSERT INTO authtoken_token (`key`, created, user_id) VALUES ('dcd52b4996698b6c5db63df6894550fe1069d2a0','1994-07-29', '1');
