df9226cbf90b1cc17ac9477346f42a264abd8d1c
Raphael Wudy <raphael.wudy@th-rosenheim.de>
2024-06-08 17:16:17 +0200

    add correct token

--- locustfile.py
+++ locustfile.py
@@ -1,11 +1,13 @@
 import time
 import os
+from dotenv import load_dotenv
 from locust import HttpUser, task, between
 import json
 
 class QuickstartUser(HttpUser):
+    load_dotenv()
     wait_time = between(1, 5)
-    header = {'Authorization': 'Bearer ae7c53dcaafe8887d331003252fa90f6c5ff5059', "Content-Type": "application/json"}
+    header = {'Authorization': 'Bearer ' + os.environ.get('TOKEN_UNIT_TEST'), "Content-Type": "application/json"}
 
     @task(1)
     def create_electricity(self):
